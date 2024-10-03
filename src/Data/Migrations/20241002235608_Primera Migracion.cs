0a9201db8eef71bf615879cf9f078c011cb86f18
Yinkov <yinko.veliz@alumnos.ucn.cl>
2024-10-02 21:09:38 -0300

    feat: add folder Controller

---
+++ src/Data/Migrations/20241002235608_Primera Migracion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace catedra1.src.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    rut = table.Column<string>(type: "TEXT", nullable: false),
+                    nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    correo = table.Column<string>(type: "TEXT", nullable: false),
+                    genero = table.Column<string>(type: "TEXT", nullable: false),
+                    fechaNacimiento = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
